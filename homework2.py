46fcaa9f53e7fb764275415a35c12ea1e726bf6b
Tetiana <selivanovatetiana@gmail.com>
2021-06-28 22:16:43 +0300

    Revert "ID: HW2"
    This reverts commit 7f5c087b

--- homework2.py
+++ homework2.py
@@ -5,10 +5,8 @@ global_logic = ['Cindy Brooks', 'Arif Perkins', 'Ryan Ferry']
 toshiba.extend(global_logic)
 
 # 2
-bingo_blacklist = {'Riccardo Lozano', 'Nicky Powell', 'Randy Morris'}
+bigno_blacklist = {'Riccardo Lozano', 'Nicky Powell', 'Randy Morris'}
 poker_blacklist = {'Mila Salter', 'Randy Morris', 'Abel Poole', 'Jem Chase'}
-mahjong_blacklist = {'Randy Morris', 'Riccardo Lozano', 'Gruffydd Vaughan'}
-
-bingo_blacklist.intersection(poker_blacklist, mahjong_blacklist)
+majong_blacklist = {'Randy Morris', 'Riccardo Lozano', 'Gruffydd Vaughan'}
 
 bigno_blacklist.intersection(poker_blacklist, majong_blacklist)
