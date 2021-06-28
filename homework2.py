becc89ac409b9cd3d0b364d45d8141648dcfb326
Tetiana <selivanovatetiana@gmail.com>
2021-06-28 22:34:47 +0300

    ID: HW2
    * Added 2 lists of guests.

--- homework2.py
+++ homework2.py
@@ -16,3 +16,23 @@ vegetarians = ['Brian Hunter', 'Osman Leon', 'Nikhil Owens']
 omnivores = ['Eben Tyson', 'Nico White', 'Darius West', 'Delia Owen']
 
 print(vegetarians + omnivores)
+
+# 4
+vip_places = {
+    'place_1': 'Carl Ho',
+    'place_2': 'Alexie Cardenas',
+    'place_3': 'Martina Carpenter',
+    'place_4': 'Alix Rooney',
+    'place_5': 'Tyrese Pena'
+}
+common_places = {
+    'place_1': 'Keeleigh Craig',
+    'place_2': '',
+    'place_3': 'Hamid Ford',
+    'place_4': 'Cairo Lugo',
+    'place_5': 'Emeli Hunt',
+    'place_6': '',
+    'place_7': 'Mya Iles',
+    'place_8': '',
+    'place_9': 'Anne Cano'
+}
