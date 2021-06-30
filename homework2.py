c5b89489fd25b4bfa6f34e3b1c90073b372239b7
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-06-30 19:50:15 +0300

    HW3: add some comments how to optimize code or description of some issues

--- homework2.py
+++ homework2.py
@@ -3,6 +3,7 @@ toshiba = ['Kara Cohen', 'Onur Boyce', 'Cindy Brooks', 'Saarah Lowe']
 global_logic = ['Cindy Brooks', 'Arif Perkins', 'Ryan Ferry']
 
 toshiba.extend(global_logic)
+# Good but employees of global now in both companies but should be in toshiba
 
 # 2
 bingo_blacklist = {'Riccardo Lozano', 'Nicky Powell', 'Randy Morris'}
@@ -10,6 +11,7 @@ poker_blacklist = {'Mila Salter', 'Randy Morris', 'Abel Poole', 'Jem Chase'}
 mahjong_blacklist = {'Randy Morris', 'Riccardo Lozano', 'Gruffydd Vaughan'}
 
 bingo_blacklist.intersection(poker_blacklist, mahjong_blacklist)
+# Good
 
 # 3
 vegetarians = ['Brian Hunter', 'Osman Leon', 'Nikhil Owens']
@@ -17,13 +19,16 @@ omnivores = ['Eben Tyson', 'Nico White', 'Darius West', 'Delia Owen']
 
 print(vegetarians + omnivores)
 
+# Good
+
 # 4
 vip_places = {
     'place_1': 'Carl Ho',
     'place_2': 'Alexie Cardenas',
     'place_3': 'Martina Carpenter',
     'place_4': 'Alix Rooney',
-    'place_5': 'Tyrese Pena'
+    'place_5': 'Tyrese Pena',
+    'place_5': 'Intruder',
 }
 common_places = {
     'place_1': 'Keeleigh Craig',
@@ -36,12 +41,20 @@ common_places = {
     'place_8': '',
     'place_9': 'Anne Cano'
 }
+# Not bad but I can easily extend vip places
 
 # 5
-people = ['Rahim Mccoy', 'John Dow', 'Dorothy King', 'Khalil Olson',
-        'Marta Dow', 'John Dow', 'Leyton Brandt', 'Dorothy King']
+people = [
+    'Rahim Mccoy', 'John Dow', 'Dorothy King', 'Khalil Olson',
+    'Marta Dow', 'John Dow', 'Leyton Brandt', 'Dorothy King'
+]
 
 for i in people:
     if people.count(i) > 1:
         people.remove(i)
 print(people)
+
+# Interesting solutions. take a look how it could be implemented in one string
+# print(list({}.fromkeys(list_of_guests).keys()))
+
+# Move all tasks in separate modules
