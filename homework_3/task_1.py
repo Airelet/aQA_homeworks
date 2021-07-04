9b39959b08afec34d3c44f8da890a1c24d01b39b
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-04 14:27:39 +0300

    HW3: add refactoring of structure and add examples of code
    * move homework_1 into the homework_1 package
    
    * split homework_3 modules on new 3 modules
    
    * move tasks from homework 3 into the related package
    
    * add alternative solutions for task 2
    
    * add description with issues in task 1 and examples of code which should help
    
    * add description with issuese in task 3 about James

---
+++ homework_3/task_1.py
@@ -0,0 +1,26 @@
+list_of_all = [1, 2, 3, 4, 5, 6, 7, 8]  # We already know that it is a list so I suggest to name it like elements
+list_of_even = []  # Same. I suggest to name it like event_elements
+list_of_odd = []  # Same. I suggest to name it like odd elements
+
+for i in range(0, len(list_of_all), 2):
+  list_of_even += (i, list_of_all[i])  # it chould be list of tuples but you have create simple list
+
+for i in range(0, len(list_of_all), 2):
+  list_of_odd.extend([i, list_of_all[i+1]])
+
+print(list_of_even)
+print(list_of_odd)
+
+# Well. Something was done in this task but incorectly. Take a look on last link for lesson 3.
+# TODO: rewrite task using single for each loop. You have used for loop instead.
+# TOOD: you should create list of tuples. You have make list and I don't understand which fron elements index and which one is element
+# Some examples of how to make list of tuples:
+# some_list.append((index, value))
+
+# Some examples of how to determine is element even or odd:
+# if index % 2 == 0:
+#     print("element is event")
+# else:
+#     print("element is odd")
+
+# Also I suggest to look on list of links which I have provide for lesson and take a look on enumarate function
---
+++ homework_3/task_2.py
@@ -0,0 +1,10 @@
+two = 2
+two_mult_1 = two * 2
+two_diff_1 = two / 2
+two_mult_2 = two ** 2
+two_diff_2 = two // 2
+
+# Good. I have not clarify which operators ;you should use.
+# But alternative solution for multiplying and dividing by 2 was bit operator
+# b = a << 1  # multiply on 2
+# e = a >> 1  # divide on 2
